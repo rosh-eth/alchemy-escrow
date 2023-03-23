f5b5b31a8b3cb9096571a587441d3c8f147926fd
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2023-03-23 12:47:24 +1100

    changed from wei to ether being sent?

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,11 @@
-const { network } = require("hardhat");
+// const { network } = require("hardhat");
 
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
-  },
-  network: {
-    goerli: {
-      url: process.env.GOERLI_URL,
-      accounts: process.env.PRIVATE_KEY,
-    },
   },
 };
