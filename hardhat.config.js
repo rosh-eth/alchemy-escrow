a4f0cd15b85e2f2b1dcf3b1f36aec46e5def767c
Roshan Ghadamian <7765692+rosh-eth@users.noreply.github.com>
2023-03-28 16:21:26 +1100

    fixing env

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,12 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
   paths: {
     artifacts: "./app/src/artifacts",
   },
